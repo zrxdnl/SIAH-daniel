811f8f4dfcc5486748b7478d7011ec8fe8290ace
zrxdnl <ezra.felix14@gmail.com>
2022-10-16 16:39:53 -0300

    commit

--- setor/urls.py
+++ setor/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListaSetorView, SetorCreateView, SetorDeleteView
+from .views import ListaSetorView, SetorCreateView, SetorDeleteView, SetorUpdateView
 
 urlpatterns = [
     path('', ListaSetorView.as_view(), name='setor.index'),
     path('novo/', SetorCreateView.as_view(), name='setor.novo'),
+    path('editar/<int:pk>', SetorUpdateView.as_view(), name='setor.editar'),
     path('remover/<int:pk>', SetorDeleteView.as_view(), name='setor.remover'),
 ]
--- setor/views.py
+++ setor/views.py
@@ -1,11 +1,21 @@
+from urllib import request
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, DeleteView
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from .models import Setor
 from .forms import SetorForm
-
 class ListaSetorView(ListView):
      model = Setor
      queryset = Setor.objects.all().order_by('nome_setor')
+     def get_queryset(self):
+          queryset = super().get_queryset()
+
+          filtro = self.request.GET.get('nome') or None
+          if filtro:
+               if filtro.isnumeric():
+                    queryset= queryset.filter(codigo_setor__contains=filtro)
+               else:
+                    queryset= queryset.filter(nome_setor__contains=filtro)                  
+          return queryset
 
 class SetorCreateView(CreateView):
      model = Setor
@@ -14,4 +24,9 @@ class SetorCreateView(CreateView):
 
 class SetorDeleteView(DeleteView):
      model = Setor
+     success_url = '/setor/'
+
+class SetorUpdateView(UpdateView):
+     model = Setor
+     form_class = SetorForm
      success_url = '/setor/'
